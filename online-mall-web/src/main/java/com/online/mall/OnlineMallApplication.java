60116d201628d55970ebe49c7b9ea1da66659821
mgcele <hong_mgcele@163.com>
2017-08-25 09:41:16 +0800

    初始化

---
+++ online-mall-web/src/main/java/com/online/mall/OnlineMallApplication.java
@@ -0,0 +1,16 @@
+package com.online.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mgcele
+ */
+@SpringBootApplication
+public class OnlineMallApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMallApplication.class);
+    }
+    
+}
